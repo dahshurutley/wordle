ce9393fe097647d078dca34c5333663d5f299e0c
Dahshur Utley <87342746+dahshurutley@users.noreply.github.com>
2022-03-30 02:06:49 +0000

    Final Updates

--- main.py
+++ main.py
@@ -1,64 +1,52 @@
-# Account for user guess being in wordList: Done 
-# If not=, subtract a guess for loop and continue..?: Done
-# Print in one line: Done
-# Last Step: Adding a word list using a package.: Done
-
-
+# Output letters that arent in word within list 
 
 from nltk.corpus import words
 import random
 from termcolor import colored
 
-
 wordList = []
+wordsVoid = set()
 for i in words.words():
   if len(i) == 5:
     wordList.append(i.upper())
-    
 wordChoice = [i for i in random.choice(wordList).upper()]
-print(wordChoice)
 guess = 0 
 
 print("Here are the rules: \nGreen Letters means they're in the correct position\nYellow Letters are in the word...but in the wrong position\nWhite Letters are not in the word\nEmpty Guesses and Words not equal to 5 letters are not counted as guesses.\n")
-
-
-
-
 
 while guess <= 6:
   guess += 1
   userInput = input('Input Word Guess: ').upper()
   userContent = [i for i in userInput]
-  print(userContent)
   emptyList = []
 
   if userContent == wordChoice:
     print(colored(f"{''.join(userContent)}", 'green'))
     print(f'Correct, the word was {"".join(wordChoice)}. It took you {guess} Tries.')
     break
-
   if userInput in wordList:
     for i in range(0, len(wordChoice)):
       if userContent[i] in wordChoice: 
         if userContent[i] == wordChoice[i]:
-          # Return Joined string of letters
           emptyList.append(colored(f'{userContent[i]}', 'green'))
         else: 
           emptyList.append(colored(f'{userContent[i]}', 'yellow'))
       else:
         emptyList.append(colored(f'{userContent[i]}', 'white'))
+        wordsVoid.add(userContent[i])
   else: 
     if len(userContent) != 5: 
-      print('Not a Five Letter Word')
+      print('Not a Five Letter Word\n')
       guess -= 1
       continue
-
     if userInput not in wordList:
-      print('Not in Word List')
+      print('Not in Word List\n')
       guess -= 1
-      
-      
+      continue
   print(''.join(emptyList))
+  print(f'Letters not in word: {sorted(wordsVoid)}\n') 
+
+print(f'The Word Was {"".join(wordChoice).lower().capitalize()}') 
 
 
 
